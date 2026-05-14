22bc89613178235d6eb0fefd1ba89e930435cd9c
Naveen <naveenramlu28@gmail.com>
2026-05-14 16:09:06 +0530

    #Firewall module created

--- dev.tfvars
+++ dev.tfvars
@@ -18,3 +18,8 @@ environment   = "dev-env"
 # Subnet 
 subnetwork_name          = "private-subnet"
 subnetwork_ip_cidr_range = "10.10.0.0/16"
+
+# Firewall 
+allow_internal_firewall_rule_name = "internal-firewall-rule"
+allow_external_firewall_rule_name = "external-firewall-rule"
+allow_gke_rule_name               = "gke-firewall-rule"
--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-module "vpc" {
-  source                  = "./modules/vpc"
-  vpc_name                = var.vpc_name
-  auto_create_subnetworks = var.auto_create_subnetworks
-  routing_mode            = var.routing_mode
-}
-
 module "gcs" {
   source        = "./modules/gcs_bucket"
   bucket_name   = var.bucket_name
@@ -13,10 +6,26 @@ module "gcs" {
   environment   = var.environment
 }
 
+module "vpc" {
+  source                  = "./modules/vpc"
+  vpc_name                = var.vpc_name
+  auto_create_subnetworks = var.auto_create_subnetworks
+  routing_mode            = var.routing_mode
+}
+
 module "subnets" {
   source                   = "./modules/subnets"
   subnetwork_name          = var.subnetwork_name
   subnetwork_ip_cidr_range = var.subnetwork_ip_cidr_range
   region_name              = var.region_name
   network_id               = module.vpc.vpc_id
+}
+
+module "firewall" {
+  source                            = "./modules/firewall"
+  network_id                        = module.vpc.vpc_id
+  allow_internal_firewall_rule_name = var.allow_internal_firewall_rule_name
+  allow_external_firewall_rule_name = var.allow_external_firewall_rule_name
+  allow_gke_rule_name               = var.allow_gke_rule_name
+  subnetwork_ip_cidr_range          = var.subnetwork_ip_cidr_range
 }
